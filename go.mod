aec643459591430a0079a2b85f785773d925345c
Juan Carlos Hernandez <juancarloshdz23@gmail.com>
2023-06-12 09:48:40 -0700

    Inicio con variables e importación de paquetes

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/juanhdz23/go-desde-0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/juanhdz23/go-desde-0/variables"
+)
+
+func main() {
+	// fmt.Println("Hola Mundo JC")
+	variables.MuestroEnteros()
+}
