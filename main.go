3b9533161ba7b614e7148a99198222b1bad289e8
Juan Carlos Hernandez <juancarloshdz23@gmail.com>
2023-06-13 15:55:46 -0700

    Commit desde bash

--- main.go
+++ main.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/juanhdz23/go-desde-0/variables"
+	"runtime"
+	// "github.com/juanhdz23/go-desde-0/variables"
 )
 
 func main() {
 	// fmt.Println("Hola Mundo JC")
 	// variables.MuestroEnteros()
 	// variables.RestoVariables()
-	estado, texto := variables.ConviertoTexto(15)
-	fmt.Println(estado)
-	fmt.Println(texto)
+	// estado, texto := variables.ConviertoTexto(15)
+	// fmt.Println(estado)
+	// fmt.Println(texto)
+
+	// os := runtime.GOOS
+	// if os=="Linux." {
+	fmt.Println(runtime.GOOS)
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("Esto no es Windows, es", os)
+	} else {
+		fmt.Println("Esto es Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
