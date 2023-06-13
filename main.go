dd8331ab48eec6600a4a55d4d3ffef973025550b
Juan Carlos Hernandez <juancarloshdz23@gmail.com>
2023-06-13 07:57:49 -0700

    Convertir números a texto

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/juanhdz23/go-desde-0/variables"
 )
 
 func main() {
 	// fmt.Println("Hola Mundo JC")
-	variables.MuestroEnteros()
+	// variables.MuestroEnteros()
+	// variables.RestoVariables()
+	estado, texto := variables.ConviertoTexto(15)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
